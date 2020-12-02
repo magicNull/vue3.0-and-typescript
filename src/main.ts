2dd9d0049a8439e3595b53299cdd914cfe6848b5
Magic <xl1207895752@gmail.com>
2020-12-03 00:39:07 +0800

    feat: 添加element plus

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// elementPlus
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
 createApp(App)
   .use(store)
   .use(router)
+  .use(ElementPlus)
   .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,15 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    test element plus
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
